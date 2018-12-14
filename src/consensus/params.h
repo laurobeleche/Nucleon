d7752a649dbda6e34192f85dbda1f9fd1e5833ff
CryptoCoderz <CryptoCoderz@gmail.com>
2018-12-13 22:33:13 -0800

    Fixed params.h referencing

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -85,7 +85,7 @@ struct Params {
     int64_t nPowTargetSpacing;
     int64_t nPowTargetTimespan;
     int64_t nPowVRXHeight;
-    int64_t nPoWVelocityHeight;
+    int64_t nPowVelocityHeight;
     int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
     uint256 nMinimumChainWork;
     uint256 defaultAssumeValid;
