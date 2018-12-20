bcecd5c1d91c758b28741ec6f8df45f876566601
CryptoCoderz <CryptoCoderz@gmail.com>
2018-12-19 18:16:45 -0800

    Version bump to 2.2.0

--- src/config/neon-config.h
+++ src/config/neon-config.h
@@ -18,7 +18,7 @@
 #define CLIENT_VERSION_MAJOR 2
 
 /* Minor version */
-#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_MINOR 2
 
 /* Build revision */
 #define CLIENT_VERSION_REVISION 0
@@ -319,7 +319,7 @@
 #define PACKAGE_NAME "Nucleon Core"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "Nucleon Core 2.0.0"
+#define PACKAGE_STRING "Nucleon Core 2.2.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "neoncore"
@@ -328,7 +328,7 @@
 #define PACKAGE_URL "https://mealworms.biz/neonhome.html"
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "2.0.0"
+#define PACKAGE_VERSION "2.2.0"
 
 /* Define to necessary symbol if this constant uses a non-standard name on
    your system. */
